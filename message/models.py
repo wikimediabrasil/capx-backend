cf96e1531f8adba3d23227b73625f18807ff5fa9
albertoleoncio <albertoleoncio@hotmail.com>
2024-12-22 14:26:02 -0300

    refactor: Move message sending logic to MessageService and update Message model to use it

--- message/models.py
+++ message/models.py
@@ -1,76 +1,7 @@
 from django.db import models
 from django.conf import settings
-from requests_oauthlib import OAuth1Session
-from social_django.models import UserSocialAuth
+from .services.message_service import MessageService
 
-
-class MessageManager(models.Manager):
-    def send_message(self, instance):
-        url = 'https://meta.wikimedia.org/w/api.php'
-        user_social_auth = UserSocialAuth.objects.get(user=instance.sender)
-        oauth = OAuth1Session(
-            settings.SOCIAL_AUTH_MEDIAWIKI_KEY,
-            client_secret=settings.SOCIAL_AUTH_MEDIAWIKI_SECRET,
-            resource_owner_key=user_social_auth.extra_data['access_token']['oauth_token'],
-            resource_owner_secret=user_social_auth.extra_data['access_token']['oauth_token_secret']
-        )
-
-        # Get tokens
-        params_token = {
-            'action': 'query',
-            'meta': 'tokens',
-            'format': 'json',
-            'formatversion': '2',
-        }
-        reply = oauth.get(url, params=params_token, timeout=60)
-        token = reply.json().get('query', {}).get('tokens', {}).get('csrftoken', '')
-
-        if not token:
-            instance.status = 'failed'
-            instance.save()
-            return
-
-        params_emailable = {
-            'action': 'query',
-            'format': 'json',
-            'list': 'users',
-            'formatversion': '2',
-            'usprop': 'emailable',
-            'ususers': instance.receiver,
-        }
-        reply = oauth.get(url, params=params_emailable, timeout=60)
-        if reply.json().get('query', {}).get('users', [{}])[0].get('emailable', False):
-            params_send = {
-                'action': 'emailuser',
-                'target': instance.receiver,
-                'subject': instance.message[:50],  # Assuming the first 50 chars as the subject
-                'text': instance.message,
-                'ccme': '1',
-                'format': 'json',
-                'token': token,
-            }
-            reply = oauth.post(url, data=params_send, timeout=60)
-            if reply.json().get('emailuser', {}).get('result') == 'Success':
-                instance.status = 'sent'
-            else:
-                instance.status = 'failed'
-            instance.save()
-        else:
-            params_send = {
-                'action': 'edit',
-                'title': f'User talk:{instance.receiver}',
-                'section': 'new',
-                'sectiontitle': instance.message[:50],  # Assuming the first 50 chars as the section title
-                'text': instance.message,
-                'format': 'json',
-                'token': token,
-            }
-            reply = oauth.post(url, data=params_send, timeout=60)
-            if reply.json().get('edit', {}).get('result') == 'Success':
-                instance.status = 'sent'
-            else:
-                instance.status = 'failed'
-            instance.save()
 
 class Message(models.Model):
     MESSAGE_METHOD = (
@@ -84,8 +15,6 @@ class Message(models.Model):
     status = models.CharField(max_length=10, default='pending')
     date = models.DateTimeField(auto_now_add=True)
 
-    objects = MessageManager()
-
     def save(self, *args, **kwargs):
         is_new = self.pk is None
         super().save(*args, **kwargs)
@@ -93,7 +22,7 @@ class Message(models.Model):
         if is_new:
             self.status = 'sending'
             self.save(update_fields=['status'])
-            Message.objects.send_message(self)
+            MessageService.send_message(self)
 
     def __str__(self):
         return f'{self.sender} to {self.receiver} - {self.date.strftime("%d/%m/%Y %H:%M:%S")}'
--- message/views.py
+++ message/views.py
@@ -6,3 +6,18 @@ from rest_framework import status, viewsets, filters
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     queryset = Message.objects.all()
+
+    # Always set field "sender" as the current user
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+    def get_serializer_class(self):
+        serializer_class = super().get_serializer_class()
+        serializer_class.Meta.read_only_fields = ('sender',)
+        return serializer_class
+
+    def update(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def partial_update(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
