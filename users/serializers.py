937a935f9cc53f46d9b65bc71e7f04d10dcd42b6
Alberto Leoncio <40328018+albertoleoncio@users.noreply.github.com>
2025-03-26 09:42:46 -0300

    Create new route: saved items (#79)

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Profile, CustomUser, LanguageProficiency
+from .models import Profile, CustomUser, LanguageProficiency, SavedItem
 from .submodels import Territory, Language, WikimediaProject, Avatar
 from orgs.models import Organization
 from drf_spectacular.utils import extend_schema_field, extend_schema_serializer
@@ -161,3 +161,56 @@ class UsersByTagSerializer(serializers.ModelSerializer):
             'username', 
             'profile_image'
         ]
+
+@extend_schema_field({
+    'type': 'integer',
+    'description': 'ID of the organization or user',
+})
+class EntityIdField(serializers.Field):
+    def get_attribute(self, instance):
+        """
+        Dynamically retrieve the entity_id based on the entity type.
+        """
+        if instance.entity == 'org':
+            return instance.related_org.id if instance.related_org else None
+        elif instance.entity == 'user':
+            return instance.related_user.id if instance.related_user else None
+
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        return data
+
+
+class SavedItemSerializer(serializers.ModelSerializer):
+    entity_id = EntityIdField()
+
+    class Meta:
+        model = SavedItem
+        fields = ['id', 'relation', 'entity', 'entity_id', 'created_at']
+        read_only_fields = ['created_at']
+
+    def validate(self, data):
+        entity = data.get('entity')
+        entity_id = data.get('entity_id')
+
+        if entity == 'org':
+            if not Organization.objects.filter(id=entity_id).exists():
+                raise serializers.ValidationError("Invalid organization ID.")
+        elif entity == 'user':
+            if not CustomUser.objects.filter(id=entity_id).exists():
+                raise serializers.ValidationError("Invalid user ID.")
+                
+        return data
+
+    def create(self, validated_data):
+        entity = validated_data['entity']
+        entity_id = validated_data.pop('entity_id')
+
+        if entity == 'org':
+            validated_data['related_org'] = Organization.objects.get(id=entity_id)
+        elif entity == 'user':
+            validated_data['related_user'] = CustomUser.objects.get(id=entity_id)
+
+        return super().create(validated_data)
