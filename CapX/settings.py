c618217303f98a8fb0a447ad9dcb87c037b16680
albertoleoncio <albertoleoncio@hotmail.com>
2025-04-24 11:50:29 -0300

    Update REST framework permission class to allow read-only access

--- CapX/settings.py
+++ CapX/settings.py
@@ -147,7 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Rest
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'knox.auth.TokenAuthentication',
