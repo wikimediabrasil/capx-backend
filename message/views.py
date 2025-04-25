40be3c34be5d494119225e816ea26fb75d5b5418
Alberto Leoncio <40328018+albertoleoncio@users.noreply.github.com>
2025-04-25 10:38:53 -0300

    Add ordering functionality to MessageViewSet (#107)

--- message/views.py
+++ message/views.py
@@ -23,6 +23,9 @@ from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiPara
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['date']
+    ordering = ['-date']
 
     def get_queryset(self):
         user = self.request.user
