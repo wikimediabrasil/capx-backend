e10cde38d1247d7931c57f9e93bb112c9c5c7660
albertoleoncio <albertoleoncio@hotmail.com>
2025-05-13 09:51:17 -0300

    Allow blank values for optional fields in LetsConnectLogSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -223,12 +223,12 @@ class SavedItemSerializer(serializers.ModelSerializer):
 
 
 class LetsConnectLogSerializer(serializers.ModelSerializer):
-    full_name = serializers.CharField(write_only=True, required=False)
-    email = serializers.CharField(write_only=True, required=False)
+    full_name = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    email = serializers.CharField(write_only=True, required=False, allow_blank=True)
     role = serializers.CharField(write_only=True, required=True)
-    area = serializers.CharField(write_only=True, required=False)
-    gender = serializers.CharField(write_only=True, required=False)
-    age = serializers.CharField(write_only=True, required=False)
+    area = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    gender = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    age = serializers.CharField(write_only=True, required=False, allow_blank=True)
 
     class Meta:
         model = LetsConnectLog
