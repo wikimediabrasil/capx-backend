cf37d01f56e112aa4a1eead856d43d3ec5d39892
albertoleoncio <albertoleoncio@hotmail.com>
2025-07-18 10:56:44 -0300

    Register additional models in admin

--- orgs/admin.py
+++ orgs/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Organization
+from .models import Organization, OrganizationType, Management, TagDiff, Document
 
 admin.site.register(Organization)
+admin.site.register(OrganizationType)
+admin.site.register(Management)
+admin.site.register(TagDiff)
+admin.site.register(Document)
