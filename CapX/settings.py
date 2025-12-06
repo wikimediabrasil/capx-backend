2ede2dcd0ea92f7a1c06f06541bc31fc98f5a03b
albertoleoncio <albertoleoncio@hotmail.com>
2025-12-06 17:45:48 -0300

    Enhance language selection and description handling in translation interface

--- CapX/settings.py
+++ CapX/settings.py
@@ -169,7 +169,7 @@ REST_KNOX = {'TOKEN_TTL': timedelta(days=30)}
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Capacity Exchange (CapX) API',
     'DESCRIPTION': 'The Capacity Exchange (CapX) is a platform for finding and connecting with fellow Wikimedians to exchange knowledge, skills, and services on a global level.',
-    'VERSION': '2.2.5',
+    'VERSION': '2.2.6',
     'SERVE_INCLUDE_SCHEMA': True,
     'SWAGGER_UI_DIST': 'SIDECAR',
     'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
