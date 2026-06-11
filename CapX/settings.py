9f9aea8a1b68e6e0cb1c08b594ab2e91a2400ffa
albertoleoncio <albertoleoncio@hotmail.com>
2026-06-11 15:10:27 -0300

    chore: Bump API version to 2.2.27 in SPECTACULAR_SETTINGS

--- CapX/settings.py
+++ CapX/settings.py
@@ -170,7 +170,7 @@ REST_KNOX = {'TOKEN_TTL': timedelta(days=30)}
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Capacity Exchange (CapX) API',
     'DESCRIPTION': 'The Capacity Exchange (CapX) is a platform for finding and connecting with fellow Wikimedians to exchange knowledge, skills, and services on a global level.',
-    'VERSION': '2.2.26',
+    'VERSION': '2.2.27',
     'SERVE_INCLUDE_SCHEMA': True,
     'SWAGGER_UI_DIST': 'SIDECAR',
     'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
